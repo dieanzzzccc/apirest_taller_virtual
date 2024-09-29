ba8432af5ab23349a4e2a39565d6aff9ee2d04ce
elDiego <dieanzzcc@gmail.com>
2024-09-29 00:34:53 -0500

    Login

--- src/controllers/clientes_controller.js
+++ src/controllers/clientes_controller.js
@@ -58,8 +58,7 @@ controller.verificar_usuario = async (req, res) => {
 
         if (result && result[0][0].p_is_valid == '1') {
             const payload = {
-                email,
-                data:result
+                email
             };
 
             // Generar el token usando el servicio
